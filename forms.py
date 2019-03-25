0a6f597819bcbaa6f83e327f19100cf5da8e5971
« <emmajouffroypro@gmail.com>
2019-03-25 23:31:03 +0100

    search bar

--- forms.py
+++ forms.py
@@ -1,8 +1,17 @@
 
 from flask_wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import SelectField
 from wtforms.validators import DataRequired
 
 
 class SearchForm(Form):
-    search = StringField('search', validators=[DataRequired()])
+    search = SelectField(choices=[('Tous', 'Tous'),
+                                  ('Agriculture et agroalimentaire',
+                                   'Agriculture et agroalimentaire'),
+                                  ('Énergie', 'Énergie'),
+                                  ('Commerce et artisanat',
+                                   'Commerce et artisanat'),
+                                  ('Tourisme', 'Tourisme'),
+                                  ('Finance et assurance', 'Finance et assurance'),
+                                  ('Télécoms et Internet', 'Télécoms et Internet'),
+                                  ('Recherche', 'Recherche')])
